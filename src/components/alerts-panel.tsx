7bb432cea44a1fcbc5bacba7bf69e2a79f260a23
Sannidhya Walzade <sannidhya.walzade@gmail.com>
2025-07-26 17:28:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <TriangleAlert cla

--- src/components/alerts-panel.tsx
+++ src/components/alerts-panel.tsx
@@ -11,7 +11,7 @@ import { Badge } from "@/components/ui/badge";
 const alerts = [
   {
     id: 1,
-    icon: <AlertTriangle className="h-4 w-4 fill-black stroke-destructive" />,
+    icon: <AlertTriangle className="h-4 w-4 fill-black" />,
     title: "High Density Alert",
     description: "Crowd density in Zone A has exceeded 90%.",
     time: "2m ago",
