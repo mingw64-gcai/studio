ffa162d798f9cae49f3773c5c46cbf26db47c036
Sannidhya Walzade <sannidhya.walzade@gmail.com>
2025-07-26 20:49:54 +0000

    Increase the size of Dashboard written in title by 50% and add a subtle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Dashboard() {
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-64">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:bg-transparent sm:px-6">
            <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
@@ -68,7 +68,7 @@ export default function Dashboard() {
             </SheetContent>
           </Sheet>
           <div className="relative ml-auto flex-1 md:grow-0">
-            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <h1 className="text-3xl font-bold">Dashboard</h1>
           </div>
           <UserNav />
         </header>
