453e2187be6ee0706970cdba12978c05c4f45852
Sannidhya Walzade <sannidhya.walzade@gmail.com>
2025-07-26 19:17:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Card className="b

--- src/app/live-video/page.tsx
+++ src/app/live-video/page.tsx
@@ -260,7 +260,7 @@ export default function LiveVideoPage() {
                         {error && <Alert variant="destructive"><AlertTitle>Error</AlertTitle><AlertDescription>{error}</AlertDescription></Alert>}
                     </CardContent>
                 </Card>
-                 <Card className="border-t-[10px] border-t-[#FBBC05] border-b-0 border-l-0 border-r-0">
+                 <Card className="border-[5px] border-[#FBBC05]">
                     <CardHeader>
                         <CardTitle>Processed Video</CardTitle>
                         <CardDescription>
