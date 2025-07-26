0aed6aac23dc45ea5d6e7a359318541e663052a7
Sannidhya Walzade <sannidhya.walzade@gmail.com>
2025-07-26 20:52:16 +0000

    Vertically align the dashboard title and the admin user logo with respec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Dashboard() {
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-64">
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:bg-transparent sm:px-6">
+        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:bg-transparent sm:px-6">
            <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
