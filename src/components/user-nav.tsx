afc62fbcddbebb12ea8608a75d4c74ac8675f08e
Sannidhya Walzade <sannidhya.walzade@gmail.com>
2025-07-26 07:23:24 +0000

    The change is appearing only for few seconds and then going back to the

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -19,7 +19,6 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="https://placehold.co/100x100" alt="@shadcn" />
             <AvatarFallback
               style={{ backgroundColor: "#EA4335", color: "white" }}
             >
